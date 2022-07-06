a1e398011b097edddf962a89e20639095867b41c
Woo-Yeol <king199276@gmail.com>
2022-07-06 13:01:56 +0900

    우열::Fix School Error

--- dicelab/school/tasks.py
+++ dicelab/school/tasks.py
@@ -72,8 +72,8 @@ def load_notionAPI_school():
     data = []
     for r in source['results']:
         title = r['properties']['Title']['title'][0]['plain_text']
-        lecture = [l['plain_text']
-                   for l in r['properties']['lecture']['rollup']['array'][0]['title']]
+        lecture = [l['title'][0]['plain_text']
+                   for l in r['properties']['lecture']['rollup']['array']]
         url = [l['url']
                for l in r['properties']['url']['rollup']['array']]
         lecture = zip(lecture, url)
